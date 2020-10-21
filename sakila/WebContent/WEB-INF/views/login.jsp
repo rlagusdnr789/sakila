1c2ef7d838bcb710f8cc08b36bd73f09e389d773
gd <gd@DESKTOP-SDMGM3L>
2020-10-21 18:21:26 +0900

    1021

--- sakila/WebContent/WEB-INF/views/login.jsp
+++ sakila/WebContent/WEB-INF/views/login.jsp
@@ -1,23 +1,25 @@
+  
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>login.jsp</title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-	<form>
-		<div>
-			<input type="text" placeholder="ID">
-		</div>
-		<div>
-			<input type="password" placeholder="PW">
-		</div>
-		<div>
-			<button type="button">Log-in</button>
-		</div>
-	</form>
+   <h1>로그인 폼</h1>
+   <form>
+      <div>
+         <input type="text" placeholder="ID">
+      </div>
+      <div>
+         <input type="password" placeholder="PW">
+      </div>
+      <div>
+         <button type="button"class="btn btn-primary" >Log-in</button>
+      </div>
+   </form>
 </body>
 </html>
--- sakila/src/sakila/controller/LoginServlet.java
+++ sakila/src/sakila/controller/LoginServlet.java
@@ -1,6 +1,7 @@
 package sakila.controller;
 
 import java.io.IOException;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,21 +9,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import sakila.service.*;
+import sakila.vo.*;
 
 @WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
-   //로그인 폼
-   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      HttpSession session = request.getSession();
-      if(session.getAttribute("loginStaff")==null) {
-         response.sendRedirect(request.getContextPath()+"/auth/IndexServlet");
-         return;
-      }
-      request.getRequestDispatcher("/WEP-INF/login.jsp").forward(request, response);
-   }
 
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-   //로그인 액션
-   }
+public class LoginServlet extends HttpServlet {
+
+	// 로그인 폼으로 이동
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute("loginStaff") != null) {
+
+			response.sendRedirect(request.getContextPath() + "/auth/IndexServlet");
+
+			return;
+
+		}
+
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+
+	}
+
+	// 로그인 액션으로 이동(예정)
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+
+			throws ServletException, IOException {
+
+	}
 
 }
